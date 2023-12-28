872c8b82a03e78aa81c654d556512115b7d0e719
abhishek sachdeva <0010abhi@gmail.com>
2023-12-28 13:18:26 +0530

    first commit

---
+++ src/components/AppMap.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+    apiKey: "AIzaSyDeBJQecxX4ZjjPL6aWLogO5bwQusF4kZw",
+    // version: "weekly",
+    libraries: ["places"]
+});
+
+const mapOptions = {
+    center: {
+        lat: 0,
+        lng: 0
+    },
+    zoom: 4
+};
+loader
+    .load()
+    .then((google) => {
+        new google.maps.Map(document.getElementById("map"), mapOptions);
+    })
+    .catch(e => {
+        console.error('Error in loading google map', e);
+
+    });
+</script>
+
+<template>
+    <div id="map"></div>
+</template>
+  
+<style scoped>
+#map {
+    height: 400px;
+}
+</style>
+  
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import AppMap from '../components/AppMap.vue'
+</script>
+
+<template>
+  <main style="width: 100%;">
+    <div>
+      <p>Interactive Map Query</p>
+      <div>
+        <textarea placeholder="Enter query for maps"></textarea>
+        <button>Click</button>
+      </div>
+      <div>
+        <AppMap />
+      </div>
+    </div>
+  </main>
+</template>
